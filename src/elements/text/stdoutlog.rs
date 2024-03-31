b8a769abdcff261886cedee6eb6daf6f3549983b
Marcus <marlhan@proton.me>
2024-03-31 12:18:17 +0200

    chore: Added license and copyright notice for all files

--- src/elements/text/stdoutlog.rs
+++ src/elements/text/stdoutlog.rs
@@ -1,3 +1,18 @@
+// Copyright (C) 2024  MAlba124 <marlhan@proton.me>
+//
+// StreamCraft is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// StreamCraft is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with StreamCraft.  If not, see <https://www.gnu.org/licenses/>.
+
 use crate::{
     debug_log,
     element_traits::{CommonFormat, Element, ElementArchitecture, ElementType, Sinks, Srcs},
