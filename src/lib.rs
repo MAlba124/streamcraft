223270f7045add6abeff2bae562c0aebc14b9ccb
Marcus <marlhan@proton.me>
2024-04-08 17:32:41 +0200

    docs: hello world example on main docs page

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,34 @@
 // You should have received a copy of the GNU General Public License
 // along with StreamCraft.  If not, see <https://www.gnu.org/licenses/>.
 
+//! # Streamcraft
+//!
+//! # Examples
+//!
+//! Here is a simple pipeline to print "hello world".
+//!```
+//!use streamcraft::{
+//!    elements::text::{stdoutlog::StdoutLog, texttestsrc::TextTestSrc},
+//!    pipeline::Pipeline,
+//!};
+//!
+//!// Create text sink element that only prints text to stdout
+//!let stdoutlog = StdoutLog::new();
+//!
+//!let mut texttest = TextTestSrc::new();
+//!// Link the printing element with out text src
+//!texttest.link_sink_element(stdoutlog).unwrap();
+//!// Set the text we wish to print
+//!texttest.set_text_to_send("Hello, World!\n".to_string());
+//!
+//!let mut pipeline = Pipeline::new(texttest);
+//!pipeline.init().unwrap();
+//!
+//!// Perform one iteration of the pipeline
+//!pipeline.iter().unwrap();
+//!```
+
+
 pub mod element_traits;
 pub mod elements;
 pub mod log;
